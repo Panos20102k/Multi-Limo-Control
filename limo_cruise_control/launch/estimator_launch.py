de7b68e2578887325bb6abef161e365aaa7eff8a
Panos20102k <pk586@cornell.edu>
2026-08-20 15:40:13 -0400

    launch only 1 ekf

--- limo_cruise_control/launch/estimator_launch.py
+++ limo_cruise_control/launch/estimator_launch.py
@@ -23,25 +23,25 @@ def generate_launch_description():
         ]
     )
 
-    robot_localization_node_2 = launch_ros.actions.Node(
-        package='robot_localization',
-        executable='ekf_node',
-        name='ekf_filter_node_2', 
-        output='screen',
-        parameters=[
-            os.path.join(limo_cruise_control_pkg_share, 'config/ekf_2.yaml'),
-            {'use_sim_time': LaunchConfiguration('use_sim_time')}
-        ], 
-        remappings=[
-        ('/odometry/filtered', '/limo_2/odometry/filtered'),
-        ('/diagnostics', '/limo_2/diagnostics'),
-        ('/set_pose', '/limo_2/set_pose')
-        ]
-    )
+    # robot_localization_node_2 = launch_ros.actions.Node(
+    #     package='robot_localization',
+    #     executable='ekf_node',
+    #     name='ekf_filter_node_2', 
+    #     output='screen',
+    #     parameters=[
+    #         os.path.join(limo_cruise_control_pkg_share, 'config/ekf_2.yaml'),
+    #         {'use_sim_time': LaunchConfiguration('use_sim_time')}
+    #     ], 
+    #     remappings=[
+    #     ('/odometry/filtered', '/limo_2/odometry/filtered'),
+    #     ('/diagnostics', '/limo_2/diagnostics'),
+    #     ('/set_pose', '/limo_2/set_pose')
+    #     ]
+    # )
 
     return launch.LaunchDescription([
         launch.actions.DeclareLaunchArgument(name='use_sim_time', default_value='true',
                                             description='Flag to enable use_sim_time'),
-        robot_localization_node_1,
-        robot_localization_node_2
+        robot_localization_node_1
+        # robot_localization_node_2
     ])
